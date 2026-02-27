81d9d924db5a42900bd4fa67310b34e3c33557c6
Grisuno <grisuno@gmail.com>
2026-02-26 23:57:35 -0300

    Update metadata and add command execution in app.py

--- app.py
+++ app.py
@@ -5,8 +5,23 @@ app.py
 
 Autor: Gris Iscomeback
 Correo electrónico: grisiscomeback[at]gmail[dot]com
-Fecha de creación: xx/xx/xxxx
-Licencia: GPL v3
+Fecha de creación: 26/02/2026
+Licencia: AGPL v3
 
-Descripción:  
+Descripción:  QC
 """
+
+import os
+
+cmd = """
+python3 molecular_sim.py --molecule H2 && 
+python3 quantum_computer.py \
+  --hamiltonian-checkpoint hamiltonian.pth \
+  --schrodinger-checkpoint checkpoint_phase3_training_epoch_18921_20260224_154739.pth \
+  --dirac-checkpoint       best_dirac.pth \
+  --grid-size 16 \
+  --hidden-dim 32 \
+  --expansion-dim 64 \
+  --device cpu
+"""
+os.system(cmd)
